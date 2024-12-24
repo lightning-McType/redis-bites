42012403f950b497ea65007da59b8eb9e3fee90a
lightning-McType <mohitsuri912@gmail.com>
2024-12-24 10:54:46 +0530

    setup zod and middleware

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import { validate } from '../middlewares/validate.js';
+import { RestaurantSchema, type Restaurant } from '../schemas/restaurant.js';
 
 const router = Router();
 
-router.get('/', async (req, res) => {
+router.post('/', validate(RestaurantSchema), async (req, res) => {
+	const data = req.body as Restaurant;
 	res.send('Hello World');
 });
 
